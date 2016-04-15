05c09389d79fd83d8831c91c903f20a9c3086c43
m-ueno <m-ueno@github.com>
2016-04-15 21:42:25 +0900

    helm

--- site-elisp/packages.el
+++ site-elisp/packages.el
@@ -200,10 +200,20 @@
     (helm-mode 1)
     (define-key helm-map (kbd "C-h") 'delete-backward-char)
     (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+    (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+    (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
     (global-set-key "\M-x" 'helm-M-x)
     (global-set-key (kbd "C-x C-f") 'helm-find-files)
     (global-set-key (kbd "C-x r") 'helm-recentf)
+    (global-set-key (kbd "C-;") 'helm-mini)
+    (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+    ;; Emulate `kill-line' in helm minibuffer
+    (setq helm-delete-minibuffer-contents-from-point t)
+    (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+      "Emulate `kill-line' in helm minibuffer"
+      (kill-new (buffer-substring (point) (field-end))))
     ))
 
 (package-install 'company)
