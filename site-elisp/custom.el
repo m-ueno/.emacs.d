e160124ffef7a4530d58e229d92148a6c5b68c94
m-ueno <m-ueno@github.com>
2016-04-24 14:22:28 +0900

    outline face

--- site-elisp/custom.el
+++ site-elisp/custom.el
@@ -14,4 +14,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit outline-1 :foreground "orange")))))
+ '(markdown-header-face-1 ((t (:inherit outline-1))))
+ '(markdown-header-face-2 ((t (:inherit outline-2))))
+ '(markdown-header-face-3 ((t (:inherit outline-3))))
+ '(org-level-1 ((t (:inherit outline-1 :foreground "orange"))))
+ '(outline-1 ((t (:inherit font-lock-constant-face))))
+ '(outline-2 ((t (:inherit font-lock-keyword-face))))
+ '(outline-3 ((t (:inherit font-lock-variable-name-face)))))
