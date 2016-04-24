8b38d6441bc09eaac5a30aa48b3f289523f945fe
m-ueno <m-ueno@github.com>
2016-04-24 14:14:39 +0900

    dim paren

--- site-elisp/configs.el
+++ site-elisp/configs.el
@@ -325,6 +325,17 @@
          )
        auto-insert-alist))
 
+;; elisp括弧を薄くする
+(defface paren-face
+  '((((class color) (background dark))
+     (:foreground "grey20"))
+    (((class color) (background light))
+     (:foreground "grey80")))
+  "Face used to dim parentheses.")
+(add-hook 'emacs-lisp-mode-hook
+ 	  (lambda ()
+ 	    (font-lock-add-keywords nil
+ 				    '(("(\\|)" . 'paren-face)))))
 
 ;; ----------------------------------------------------------------
 ;; key binding
